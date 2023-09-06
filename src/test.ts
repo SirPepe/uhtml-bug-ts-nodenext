eace7044c46a9f5146e02bc32256c179f3e6ea8f
Peter Kröner <peter@peterkroener.de>
2023-09-06 09:35:14 +0200

    :hatching_chick:

---
+++ src/test.ts
@@ -0,0 +1,4 @@
+import { html, render } from "uhtml";
+
+// Could not find a declaration file for module 'uhtml'. '/home/peter/uhtml-bug-ts-nodenext/node_modules/uhtml/esm/index.js' implicitly has an 'any' type.
+// There are types at '/home/peter/uhtml-bug-ts-nodenext/node_modules/uhtml/index.d.ts', but this result could not be resolved when respecting package.json "exports". The 'uhtml' library may need to update its package.json or typings.ts(7016)
